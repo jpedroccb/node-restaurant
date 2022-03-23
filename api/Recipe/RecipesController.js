727f23bbe198aac1cb89483e00493fe3f59a4b8e
joao.pedro6 <joao.pedro@ubistart.com>
2022-03-23 00:20:16 -0300

    WIP: Recipe Index with Preload Ingredients

--- api/Recipe/RecipesController.js
+++ api/Recipe/RecipesController.js
@@ -1,3 +1,5 @@
+const Ingredient = require('../models/IngredietModel')
+const Recipe = require('../models/RecipeModel')
 const RecipeService = require('./Service/RecipeService')
 
 exports.store = async (req, res) => {
@@ -9,7 +11,12 @@ exports.store = async (req, res) => {
 }
 
 exports.index = async (req, res) => {
-
+    return res.status(200).send(await Recipe.findAll({
+        include: {
+            model: Ingredient,
+            as: 'ingredients'
+        }
+    }))
 }
 
 exports.show = async (req, res) => {
